a7fb8377efdba25c1cbb6cbe0c99ae9286c951d7
Hasnain Ali <hasnain095@github.com>
2023-06-02 19:18:46 +0500

    fix

--- greeting/settings/production.py
+++ greeting/settings/production.py
@@ -1,5 +1,2 @@
 def plugin_settings(settings):
-    # Update the provided settings module with any app-specific settings.
-    # For example:
-    #     settings.FEATURES['ENABLE_MY_APP'] = True
-    #     settings.MY_APP_POLICY = 'foo'
+    pass
