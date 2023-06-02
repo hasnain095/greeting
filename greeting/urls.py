99d5f2126d2419a25aa72426c065a8e1dedfc2e9
Hasnain Ali <hasnain095@github.com>
2023-06-02 23:58:29 +0500

    fix

--- greeting/urls.py
+++ greeting/urls.py
@@ -8,5 +8,5 @@ from .views import greeting
 
 urlpatterns = [
     # re_path(r'', TemplateView.as_view(template_name="greeting/base.html")),
-    path('v1/greeting/', greeting, name='greeting'),
+    path('api/v1/greeting/', greeting, name='greeting'),
 ]
