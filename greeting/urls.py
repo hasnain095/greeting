727aa8fccc335cf1b8de9a73ca641fafcbd009e0
Hasnain Ali <hasnain095@github.com>
2023-06-02 23:11:53 +0500

    fix

--- greeting/urls.py
+++ greeting/urls.py
@@ -4,7 +4,7 @@ URLs for greeting.
 from django.urls import re_path , path # pylint: disable=unused-import
 from django.conf.urls import url
 from django.views.generic import TemplateView  # pylint: disable=unused-import
-from .view import greeting
+from .views import greeting
 
 urlpatterns = [
     # re_path(r'', TemplateView.as_view(template_name="greeting/base.html")),
