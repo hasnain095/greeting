d38b933dcc9623a31cb487d47eef67ec05928d1b
Hasnain Ali <hasnain095@github.com>
2023-06-05 18:03:39 +0500

    fix

--- greeting/urls.py
+++ greeting/urls.py
@@ -2,7 +2,7 @@
 URLs for greeting.
 """
 from django.conf.urls import url
-from .views import Greetingy, greeting_func
+from .views import Greeting, greeting_func
 
 urlpatterns = [
     url(r'^v1/greeting/$', Greeting.as_view(), name='greeting'),
