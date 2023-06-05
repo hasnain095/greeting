28b8c1bece064e112123521ad9b12307aa5254ce
Hasnain Ali <hasnain095@github.com>
2023-06-05 19:30:28 +0500

    fix

--- greeting/urls.py
+++ greeting/urls.py
@@ -2,9 +2,11 @@
 URLs for greeting.
 """
 from django.conf.urls import url
-from .views import Greeting, greeting_func
+from .views import Greeting, greeting_func, login, authorize
 
 urlpatterns = [
     url(r'^v1/greeting/$', Greeting.as_view(), name='greeting'),
     url(r'^v2/greeting/$', greeting_func, name='greeting'),
+    url(r'^v1/login/$', login, name="login"),
+    url(r'^v1/authorize', authorize, name="authorize")
 ]
--- greeting/views.py
+++ greeting/views.py
@@ -28,3 +28,23 @@ class Greeting(View):
             return JsonResponse({"greeting": "hello to you too"})
         else:
             return JsonResponse({"greeting": f"{value} to you too"})
+
+
+def login(request):
+    # build a full authorize callback uri
+    redirect_uri = request.build_absolute_uri('/authorize/')
+    return oauth.lms.authorize_redirect(request, redirect_uri)
+
+
+def authorize(request):
+    # This will create a http request client that points to the LMS.
+    lms = oauth.create_client('lms')
+    # Here, we authenticate the client with the token we got from the LMS. In a real-world
+    # application, we'd save this token somehow for subsequent requests.
+    token = lms.authorize_access_token(request)
+    # And then, we use this token to fetch the user's info.
+    resp = lms.get('/api/user/v1/me', token=token)
+    resp.raise_for_status()
+    profile = resp.json()
+    # Now that we have the user's info, we can render a page with the relevant info.
+    return render(request, 'authorize.html', {'profile': profile})
