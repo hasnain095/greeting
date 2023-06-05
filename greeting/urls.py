5e710fd5de28aacc863005dfe9c9502887996852
Hasnain Ali <hasnain095@github.com>
2023-06-05 18:01:34 +0500

    fix

--- greeting/urls.py
+++ greeting/urls.py
@@ -2,8 +2,9 @@
 URLs for greeting.
 """
 from django.conf.urls import url
-from .views import Greeting
+from .views import Greetingy, greeting_func
 
 urlpatterns = [
     url(r'^v1/greeting/$', Greeting.as_view(), name='greeting'),
+    url(r'^v2/greeting/$', greeting_func, name='greeting'),
 ]
--- greeting/views.py
+++ greeting/views.py
@@ -1,7 +1,22 @@
 import json
 
 from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 from django.views import View
+
+from oauth2_provider.decorators import protected_resource
+
+
+@require_http_methods(["POST"])
+@protected_resource(scopes=['user_id'])
+def greeting_func():
+    if request.method == "POST":
+        json_data = json.loads(request.body)
+        value = json_data.get("greeting", None)
+        if value == "hello":
+            return JsonResponse({"greeting": "hello to you too"})
+        else:
+            return JsonResponse({"greeting": f"{value} to you too"})
 
 
 class Greeting(View):
